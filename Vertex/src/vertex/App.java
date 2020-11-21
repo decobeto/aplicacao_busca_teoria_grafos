bdad0ce4f22fb95fc0fa60bf3d946fe3ecf6b004
André Roberto <andreroberto52@gmail.com>
2020-11-21 15:34:13 -0300

    Alterações no arquivo app para alterar grafo

--- Vertex/src/vertex/App.java
+++ Vertex/src/vertex/App.java
@@ -16,12 +16,12 @@ public class App {
         graph.add(new Vertex("4"));
         graph.add(new Vertex("5"));
         
-        graph.get(2).addNeighbor(graph.get(3));
-        graph.get(3).addNeighbor(graph.get(1));
-        graph.get(4).addNeighbor(graph.get(0));
-        graph.get(4).addNeighbor(graph.get(1));
-        graph.get(5).addNeighbor(graph.get(0));
-        graph.get(5).addNeighbor(graph.get(2));
+        graph.get(0).addNeighbor(graph.get(3));
+        graph.get(1).addNeighbor(graph.get(3));
+        graph.get(1).addNeighbor(graph.get(4));
+        graph.get(2).addNeighbor(graph.get(4));
+        graph.get(4).addNeighbor(graph.get(5));
+        graph.get(3).addNeighbor(graph.get(5));
         
         for(int i=0; i<graph.size(); i++){
             if(!graph.get(i).isVisited()){
